2cfba15b6365f534b8f6d4df973328574fcafc83
Francheskacisterna <166559994+Francheskacisterna@users.noreply.github.com>
2024-07-06 01:28:39 -0400

    Se mejora inicio de sesión y menu

---
+++ user_profesor/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import menu_prof, home_prof, planes_prof, nosotros_prof, contactos_prof
+
+urlpatterns = [
+    path('menu_prof', menu_prof, name="menu_prof"),
+    path('home_prof', home_prof, name="home_prof"),
+    path('planes_prof', planes_prof, name="planes_prof"),
+    path('nosotros_prof', nosotros_prof, name='nosotros_prof'),
+    path('contactos_prof', contactos_prof, name='contactos_profe'),
+]
--- user_profesor/views.py
+++ user_profesor/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def menu_prof(request):
+    request.session["usuario"] = "b"
+    usuario = request.session["usuario"]
+    context = {'usuario': usuario}
+    return render(request, 'user_prof/menu_adm.html', context)
+    
+@login_required
+def home_prof(request):
+    context = {}
+    return render(request, 'user_prof/home_prof.html', context)
+
+def planes_prof(request):
+    context = {}
+    return render(request, 'user_prof/planes_prof.html', context)
+
+def nosotros_prof(request):
+    context = {}
+    return render(request, 'user_prof/nosotros_prof.html', context)
+
+def contactos_prof(request):
+    context = {}
+    return render(request, 'user_prof/contactos_prof.html', context)
+
